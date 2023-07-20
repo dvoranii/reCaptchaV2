5ca04764802b9a28d6b2306db5f38e70840b5568
Ildi Dvorani <dvoranii@ildidev.com>
2023-07-20 17:13:26 -0400

    finished most of the hockey video animations

--- public/services/services.js
+++ public/services/services.js
@@ -1,6 +1,13 @@
 let video = document.querySelector(".hockey-video");
 let hockeyVideoWrapper = document.querySelector(".hockey-video-wrapper");
 let testP = document.querySelector(".test-p");
+let testP2 = document.querySelector(".test-p-2");
+
+let backgroundDiv1 = document.querySelector(".background-div-1");
+let backgroundDiv2 = document.querySelector(".background-div-2");
+let backgroundDiv3 = document.querySelector(".background-div-3");
+
+const logo = document.querySelector(".hockey-vid-logo");
 
 let options = {
   root: null,
@@ -16,10 +23,36 @@ function handleIntersect(entries, observer) {
       video.play();
 
       testP.classList.add("animate-text");
+
+      setTimeout(() => {
+        testP.classList.add("fade-out");
+        setTimeout(() => {
+          testP2.classList.add("animate-text");
+          backgroundDiv1.classList.add("animate-after");
+          backgroundDiv2.classList.add("animate-after");
+
+          // Add fade-in animation to backgroundDiv3 after 4.5 seconds
+          setTimeout(() => {
+            backgroundDiv3.classList.add("fade-in-only");
+            setTimeout(() => {
+              logo.classList.add("fade-in-logo");
+            }, 500);
+          }, 3750);
+        }, 2500);
+      }, 1500);
+
+      backgroundDiv1.classList.add("animate-after");
     } else {
       video.pause();
       video.currentTime = 0;
       testP.classList.remove("animate-text");
+      testP2.classList.remove("animate-text");
+      testP.classList.remove("fade-out");
+      testP2.classList.remove("fade-out");
+      backgroundDiv1.classList.remove("animate-after");
+      backgroundDiv2.classList.remove("animate-after");
+      backgroundDiv3.classList.remove("fade-in-only");
+      logo.classList.remove("fade-in-logo");
     }
   });
 }
